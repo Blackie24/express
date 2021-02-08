f046f2876ad8014bc56ec2f23d38ee4adc040a6f
Kacper <you@example.com>
2021-02-08 11:06:07 +0100

    day 2

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,20 @@
 var express = require("express");
+const app = require("../app");
 var router = express.Router();
+
+router.all("*", (req, res, next) => {
+  if (!req.session.admin) {
+    res.redirect("login");
+
+    return;
+  }
+
+  next();
+});
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
+  console.log(req.session.admin);
   res.render("admin", { title: "Admin" });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require("express");
 var router = express.Router();
+const login = "admin";
+const password = "123";
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", (req, res, next) => {
   res.render("index", { title: "Express" });
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("login", { title: "Logowanie" });
+});
+
+router.post("/login", (req, res, next) => {
+  const body = req.body;
+  if (body.login === login && body.password === password) {
+    req.session.admin = 1;
+    console.log(req.body);
+    res.redirect("/admin");
+  }
+  res.redirect("/login");
 });
 
 module.exports = router;
